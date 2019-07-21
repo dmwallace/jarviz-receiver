2bcdeaeae6c476c88be9d869a67d421a6f0ea1f4
Dave Wallace <davewallace@gmail.com>
2019-07-21 22:15:28 +1000

    window control experiments

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install -y nodejs.install --version 10.15.3
+choco install -y git -params '"/GitAndUnixToolsOnPath"'
+RefreshEnv
+npm install pm2@latest -g
+npm install
