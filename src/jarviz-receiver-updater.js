472452e111415184c93046ac09989418a6425257
Dave Wallace <davewallace@gmail.com>
2019-07-22 16:14:45 +1000

    window control experiments

--- src/jarviz-receiver-updater.js
+++ src/jarviz-receiver-updater.js
@@ -1,5 +1,16 @@
-const { execSync } = require('child_process')
+const { spawn } = require('child_process')
 
 setInterval(function() {
-  execSync('git', ['pull'], { windowsHide: true })
+  const cp = spawn('git', ['pull'], { windowsHide: true })
+  cp.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  cp.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  
+  cp.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 }, 10000)
