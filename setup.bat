0f2f7e352605c7b31575c3b16aab2a34e0ff9dca
Dave Wallace <davewallace@gmail.com>
2019-07-24 01:12:53 +1000

    2.0.64

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
+schtasks /delete /TN jarviz-receiver /F
 taskkill /F /IM node.exe
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+runas /user:administrator @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install -y nodejs.install --version 10.15.3
 choco install -y git -params '"/GitAndUnixToolsOnPath"'
 RefreshEnv
@@ -8,5 +9,11 @@ mkdir c:\jarviz-receiver
 cd c:\jarviz-receiver
 git clone https://github.com/dmwallace/jarviz-receiver.git .
 git checkout window-control
+if exist C:\etc\.pm2\ rmdir C:\etc\.pm2\ /q /s
+SET PM2_HOME=C:\etc\.pm2
+SETX PM2_HOME C:\etc\.pm2 /m
+npm install pm2@latest -g
+npm install pm2-windows-startup -g
+pm2-startup install
 npm install
 pause
