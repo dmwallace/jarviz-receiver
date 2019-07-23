4529de734d631e7832e2abc503201c33a204cf28
Dave Wallace <davewallace@gmail.com>
2019-07-23 16:06:16 +1000

    2.0.53

--- src/jarviz-receiver-updater.js
+++ src/jarviz-receiver-updater.js
@@ -9,7 +9,7 @@ setInterval(function() {
   }
   
   isUpdating = true
-  const cp = spawn('git', ['pull'], { windowsHide: true })
+  const cp = spawn('git reset --hard && git pull', { shell: true, windowsHide: true })
   cp.stdout.on('data', (data) => {
     //console.log(`stdout: ${data}`);
   });
