e6e273f6141264fdaaa3cc7df63deeae9bbaa015
Dave Wallace <davewallace@gmail.com>
2019-07-23 17:06:44 +1000

    2.0.61

--- setup.bat
+++ setup.bat
@@ -1,9 +1,15 @@
+taskkill /F /IM node.exe
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install -y nodejs.install --version 10.15.3
 choco install -y git -params '"/GitAndUnixToolsOnPath"'
 RefreshEnv
-git clone https://github.com/dmwallace/jarviz-receiver.git
-cd jarviz-receiver
+if exist c:\jarviz-receiver\ rmdir c:\jarviz-receiver\ /q /s
+mkdir c:\jarviz-receiver
+cd c:\jarviz-receiver
+git clone https://github.com/dmwallace/jarviz-receiver.git .
 git checkout window-control
 npm install pm2@latest -g
+npm install pm2-windows-startup -g
+pm2-startup install
 npm install
+pause
