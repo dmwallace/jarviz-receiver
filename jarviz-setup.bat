720c6cc2dc9a8deca3923b1e698ad99ba2070f31
Dave Wallace <davewallace@gmail.com>
2019-08-15 12:46:38 +1000

    2.0.87

--- jarviz-setup.bat
+++ jarviz-setup.bat
@@ -5,7 +5,7 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
  & choco upgrade -y git -params '"/GitAndUnixToolsOnPath"' ^
  & choco upgrade -y python2 visualstudio2017-workload-vctools ^
  & choco upgrade -y vscode ^
- & RefreshEnv ^
+ & set PATH="C:\ProgramData\npm;C:\Program Files\nodejs;C:\Program Files\Git\cmd\;Windows\System32;%PATH%" ^
  & rmdir c:\jarviz-receiver\ /q /s ^
  & mkdir c:\jarviz-receiver ^
  & cd c:\jarviz-receiver ^
@@ -14,25 +14,24 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
  & rmdir C:\etc\.pm2\ /q /s ^
  & mkdir C:\etc ^
  & mkdir C:\etc\.pm2 ^
- & icacls C:\etc\.pm2 /grant "Authenticated Users":RX /T ^
- & setx /M PM2_HOME C:\etc\.pm2 ^
- & setx /M PATH "C:\ProgramData\npm;%PATH%" ^
+ & "C:\Windows\System32\icacls" C:\etc\.pm2 /grant "Authenticated Users":RX /T ^
+ & "C:\Windows\System32\setx" /M PM2_HOME C:\etc\.pm2 ^
+ & "C:\Windows\System32\setx" /M PATH "C:\ProgramData\npm;C:\Program Files\nodejs;%SystemRoot%\System32;%PATH%" ^
  & set PM2_HOME=C:\etc\.pm2 ^
- & set PATH="C:\ProgramData\npm;%PATH%" ^
  & rmdir %APPDATA%\npm /q /s ^
  & mkdir C:\ProgramData\npm\npm-cache ^
  & mkdir C:\ProgramData\npm\npm ^
  & mkdir C:\ProgramData\npm\npm\node_modules ^
- & icacls C:\ProgramData\npm\npm-cache /grant "Authenticated Users":RX ^
- & icacls C:\ProgramData\npm\npm /grant "Authenticated Users":RX ^
- & icacls C:\ProgramData\npm\npm\node_modules /grant "Authenticated Users":RX ^
- & icacls C:\ProgramData\npm\npm-cache /grant "Administrators":M ^
- & icacls C:\ProgramData\npm\npm /grant "Administrators":M ^
- & icacls C:\ProgramData\npm\npm\node_modules /grant "Administrators":M ^
+ & "C:\Windows\System32\icacls" C:\ProgramData\npm\npm-cache /grant "Authenticated Users":RX ^
+ & "C:\Windows\System32\icacls" C:\ProgramData\npm\npm /grant "Authenticated Users":RX ^
+ & "C:\Windows\System32\icacls" C:\ProgramData\npm\npm\node_modules /grant "Authenticated Users":RX ^
+ & "C:\Windows\System32\icacls" C:\ProgramData\npm\npm-cache /grant "Administrators":M ^
+ & "C:\Windows\System32\icacls" C:\ProgramData\npm\npm /grant "Administrators":M ^
+ & "C:\Windows\System32\icacls" C:\ProgramData\npm\npm\node_modules /grant "Administrators":M ^
  & "C:\Program Files\nodejs\npm" config --global set prefix "C:\ProgramData\npm" ^
- & npm config --global set prefix "C:\ProgramData\npm" ^
- & npm install -g node-gyp ^
- & npm install -g pm2@3 ^
- & schtasks /delete /TN jarviz-receiver /F ^
- & npm install
+ & "C:\Program Files\nodejs\npm" config --global set prefix "C:\ProgramData\npm" ^
+ & "C:\Program Files\nodejs\npm" install -g node-gyp ^
+ & "C:\Program Files\nodejs\npm" install -g pm2@3 ^
+ & "C:\Windows\System32\schtasks" /delete /TN jarviz-receiver /F ^
+ & "C:\Program Files\nodejs\npm" install
 pause
