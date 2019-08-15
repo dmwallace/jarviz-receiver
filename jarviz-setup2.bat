eef107271e67d3959a768117913f9a13b5c6e159
Dave Wallace <davewallace@gmail.com>
2019-08-15 13:14:51 +1000

    2.0.96

--- jarviz-setup2.bat
+++ jarviz-setup2.bat
@@ -2,7 +2,7 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
  rmdir C:\etc\.pm2\ /q /s ^
  & mkdir C:\etc ^
  & mkdir C:\etc\.pm2 ^
- & icalcs C:\etc\.pm2 /grant "Authenticated Users":RX /T ^
+ & icacls C:\etc\.pm2 /grant "Authenticated Users":RX /T ^
  & setx /M PM2_HOME C:\etc\.pm2 ^
  & setx /M PATH "C:\ProgramData\npm;%PATH%" ^
  & set PM2_HOME=C:\etc\.pm2 ^
@@ -11,12 +11,12 @@ if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
  & mkdir C:\ProgramData\npm\npm-cache ^
  & mkdir C:\ProgramData\npm\npm ^
  & mkdir C:\ProgramData\npm\npm\node_modules ^
- & icalcs C:\ProgramData\npm\npm-cache /grant "Authenticated Users":RX ^
- & icalcs C:\ProgramData\npm\npm /grant "Authenticated Users":RX ^
- & icalcs C:\ProgramData\npm\npm\node_modules /grant "Authenticated Users":RX ^
- & icalcs C:\ProgramData\npm\npm-cache /grant "Administrators":M ^
- & icalcs C:\ProgramData\npm\npm /grant "Administrators":M ^
- & icalcs C:\ProgramData\npm\npm\node_modules /grant "Administrators":M ^
+ & icacls C:\ProgramData\npm\npm-cache /grant "Authenticated Users":RX ^
+ & icacls C:\ProgramData\npm\npm /grant "Authenticated Users":RX ^
+ & icacls C:\ProgramData\npm\npm\node_modules /grant "Authenticated Users":RX ^
+ & icacls C:\ProgramData\npm\npm-cache /grant "Administrators":M ^
+ & icacls C:\ProgramData\npm\npm /grant "Administrators":M ^
+ & icacls C:\ProgramData\npm\npm\node_modules /grant "Administrators":M ^
  & npm config --global set prefix "C:\ProgramData\npm" ^
  & npm config --global set prefix "C:\ProgramData\npm" ^
  & npm install -g node-gyp ^
